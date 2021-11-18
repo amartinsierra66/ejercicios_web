8677386466a8904eeaf3ceaaba75e4c5c7c8160b
Antonio <Antonio@DESKTOP-J8ARSSP.mshome.net>
2021-11-18 11:15:43 +0100

    proyecto 35

---
+++ 35_libreria_mvc_v3_filtro_profe/src/controller/LibrosAction.java
@@ -0,0 +1,43 @@
+package controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.Libro;
+import service.LibreriaFactory;
+import service.LibrosService;
+import service.TemasService;
+
+
+/**
+ * Servlet implementation class LibrosAction
+ */
+@WebServlet("/LibrosAction")
+public class LibrosAction extends HttpServlet {
+	
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int idTema=Integer.parseInt(request.getParameter("idTema"));
+	
+		LibrosService glibros=LibreriaFactory.getLibrosService();
+		TemasService gtemas=LibreriaFactory.getTemasService();
+		List<Libro> libros;
+		//si idTema es 0 es que ha elegido todos
+		if(idTema==0){
+			libros=glibros.recuperarLibros();
+		}
+		else{
+			libros=glibros.recuperarLibros(idTema);
+		}
+		request.setAttribute("libros", libros);
+		//esto se hace para que en la página de libros se pueda saber que 
+		//nombre de tema se seleccionó
+		request.setAttribute("tema", idTema>0?gtemas.recuperarTemaPorId(idTema).getTema():"Todos");
+	}
+
+}
